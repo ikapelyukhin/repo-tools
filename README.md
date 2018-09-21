4f5e7b23c6a0a1b781509bf2394b3f4d6cbd781f
Ivan Kapelyukhin <ikapelyukhin@users.noreply.github.com>
2018-09-21 11:56:59 +0200

    Verify delta-RPMs in verify_repo; update README

--- README.md
+++ README.md
@@ -6,15 +6,32 @@
 2. `bundle install`
 3. Run scripts!
 
+#### verify_repo
+
+`verify_repo.rb` verifies that all packages in the repository have a correct size. For all those
+times when somebody uploads broken repos to the CDN (╯°□°）╯︵ ┻━┻
+
+It does so by issuing `HEAD` requests and comparing `Content-Length` response header with the size
+specified in the metadata file. The requests are issued in parallel, so this is the quick way to check
+if the repo contains any malformed files.
+
+The only other way to verify the integrity of the repo would be to download every package and
+calculate checksums.
+
+```
+Usage: verify_repo.rb REPO_URL
+Authentication token can be supplied as query string, e.g.: https://repo.url/?auth_token
+```
+
 #### repo_stats
+
+`repo_stats.rb` shows repository statistics based on the metadata. For all those times when you
+need to know how much space the repository will take before mirroring it.
 
 ```
 Usage: repo_stats.rb REPO_URL
 Authentication token can be supplied as query string, e.g.: https://repo.url/?auth_token
 ```
-
-`repo_stats.rb` shows repository statistics based on the metadata. For all those cases when you
-need to know how much space the repository will take before mirroring it.
 
 ```
 Statistics for https://download.opensuse.org/update/leap/42.3/oss/
@@ -25,22 +42,3 @@ Statistics for https://download.opensuse.org/update/leap/42.3/oss/
 
 Total size:  66.60 gigabytes
 ```
-
-#### verify_repo
-
-```
-Usage: verify_repo.rb REPO_URL
-Authentication token can be supplied as query string, e.g.: https://repo.url/?auth_token
-```
-
-`verify_repo.rb` verifies that all packages in the repository have a correct size. It does so by
-issuing `HEAD` requests and comparing `Content-Length` response header with the size specified in
-the metadata file. The requests are issued in paralel, so this is the quick way to check if the
-repo contains any malformed files.
-
-The only other way to verify the integrity of the repo would be to download every package and
-calculate checksums.
-
-**Caveats:**
-
-* Doesn't verify delta-RPM packages (yet)
--- verify_repo.rb
+++ verify_repo.rb
@@ -13,6 +13,7 @@ class RepoVerifier
     repo_url += '/' unless repo_url =~ /\/$/
     @repo_url = repo_url
     @token = token
+    @broken_files = []
   end
 
   def verify
@@ -25,18 +26,37 @@ class RepoVerifier
       metadata_files[xml_file.type] << xml_file if %i[primary deltainfo].include?(xml_file.type)
     end
 
+    puts "Repo: #{@repo_url}\n"
+
+    puts "Verifying RPMs\n\n"
+    verify_files(metadata_files[:primary], RepomdParser::PrimaryXmlParser)
+
+    puts "Verifying delta-RPMs\n\n"
+    verify_files(metadata_files[:deltainfo], RepomdParser::DeltainfoXmlParser)
+
+    if @broken_files.empty?
+      puts 'All files have correct size. Hooray!'
+    else
+      puts "Packages with wrong file size:\n"
+      puts @broken_files.join("\n")
+    end
+  ensure
+    FileUtils.rm_r(@tempdir)
+  end
+
+  protected
+
+  def verify_files(metadata_files, klass)
     hydra = Typhoeus::Hydra.new
     pb = ProgressBar.new
     pb.max = 0
 
-    broken_files = []
-
-    metadata_files[:primary].each do |xml_file|
+    metadata_files.each do |xml_file|
       filename = File.basename(xml_file.location)
 
       primary_xml_file = get(xml_file.location)
 
-      rpms = RepomdParser::PrimaryXmlParser.new(primary_xml_file).parse
+      rpms = klass.new(primary_xml_file).parse
 
       rpms.each do |package|
         uri = URI.join(@repo_url, package.location)
@@ -48,7 +68,7 @@ class RepoVerifier
             actual_size = response.headers["Content-Length"].to_i
 
             if (actual_size != package.size.to_i)
-              broken_files << "#{package.location}, actual size: #{actual_size}, metadata size: #{package.size}"
+              @broken_files << "#{package.location}, actual size: #{actual_size}, metadata size: #{package.size}"
             end
 
             pb.increment!
@@ -62,20 +82,9 @@ class RepoVerifier
       end
     end
 
-    puts "Repo: #{@repo_url}\n\n"
     hydra.run
     puts ''
-    if broken_files.empty?
-      puts 'All files have correct size. Hooray!'
-    else
-      puts "Packages with wrong file size:\n"
-      puts broken_files.join("\n")
-    end
-  ensure
-    FileUtils.rm_r(@tempdir)
   end
-
-  protected
 
   def get(filename)
     uri = URI.join(@repo_url, filename)
